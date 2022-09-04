f3026a31217ff8403b6e4cbf3769e8bedf0813d9
mTamborrino <michele.tamborrino@studenti.unisalento.it>
2022-09-04 19:11:48 +0200

    Fourth Commit part 3 (New Model page)

--- frontend/static/js/views/Model.js
+++ frontend/static/js/views/Model.js
@@ -12,7 +12,19 @@ export default class extends AbstractView {
         return `
             <h1>About the model</h1>
             <p>
-                To learn from data, Machine Learning has been used.
+                The dataset is a csv file of more than 25000 rows. The columns are: Heart Rate, Respiration Rate, Oxygen's
+                Level and Body Temperature. As explained in the python colab, the 0 entries have been substituted with
+                their mean.
+            </p>
+            
+            <p>
+                For the model the random forest classifier has been used. A random forest is a meta estimator that fits
+                a number of decision tree classifiers on various sub-samples of the dataset and uses averaging to 
+                improve the predictive accuracy and control over-fitting.
+            </p>
+            
+            <p>
+                Afterwards, some fine tuning has been applied. The final test accuracy is 0.99%.
             </p>
         `;
     }
