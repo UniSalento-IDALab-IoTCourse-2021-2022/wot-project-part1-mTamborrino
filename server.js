5492ccadf73fccd2f0723b9dab50184c30c91ecf
mTamborrino <michele.tamborrino@studenti.unisalento.it>
2022-09-07 17:32:04 +0200

    Eight Commit part 3 (merge with part 1 and 2)

--- server.js
+++ server.js
@@ -1,8 +1,73 @@
 const express = require("express");
 const path = require("path");
-
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb://localhost/TemperatureDB';
+const WebSocket =  require('ws');
 
 const app = express();
+
+const wss = new WebSocket.Server({ port: 3001 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+    ws.send('something');
+});
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json());
+app.post("/temperature", (req, res, next) => {
+    console.log('Achieved '+req.body.temperature+'°C of body temperature.');
+    var sensor = req.body.sensor;
+    var temperature = req.body.temperature;
+    var heartrate = req.body.heartrate;
+    var resprate = req.body.resprate;
+    var oxygensat = req.body.oxygensat;
+    var timestamp = req.body.timestamp;
+
+    let values = [temperature, heartrate, resprate, oxygensat, timestamp]
+
+    async function pushInDb() {
+        const client = new MongoClient(uri, {useUnifiedTopology: true});
+        try {
+
+            await client.connect();
+
+            const database = client.db("TemperatureDB");
+            const temperatureColl = database.collection("temperature");
+            // create a document to be inserted
+            const doc = {
+                sensor: sensor,
+                body_temperature: temperature,
+                heart_rate: heartrate,
+                respiration_rate: resprate,
+                oxygen_saturation: oxygensat,
+                timestamp: timestamp
+            };
+
+            const result = await temperatureColl.insertOne(doc);
+        } finally {
+            await client.close();
+        }
+    }
+
+    pushInDb().catch(console.dir);
+    async function pushToClient(){
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(temperature);
+            }
+        });
+    }
+    pushToClient().catch(console.dir);
+    res.sendStatus(200)
+});
 
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
