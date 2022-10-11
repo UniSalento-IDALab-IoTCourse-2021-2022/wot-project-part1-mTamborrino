70f3aac54293417c526668caabff492fced4f3a2
samueleancora <samuele.ancora@studenti.unisalento.it>
2022-10-11 18:48:06 +0200

    Dashboard editing

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -20,9 +20,6 @@ export default class extends AbstractView {
             <p>
                 All the data gathered are visible here. Along with them, some charts are plotted.
             </p>
-            <p>
-                <a href="/model" data-link>View information about the model.</a>.
-            </p>
         `;
     }
 }
